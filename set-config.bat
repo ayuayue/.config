38e59985ce562d8edd5b3700f46196da24c6d27e
caoayu <1401262639@qq.com>
2024-11-04 13:48:01 +0800

    更新PowerShell和wezterm配置

--- set-config.bat
+++ set-config.bat
@@ -4,3 +4,6 @@ mklink "%USERPROFILE%\AppData\Roaming\nushell\env.nu" "%current_dir%\nushell\env
 mklink "%USERPROFILE%\AppData\Roaming\nushell\config.nu" "%current_dir%\nushell\config.nu" 
 mklink "%USERPROFILE%\.config\starship.toml" "%current_dir%\starship\starship.toml" 
 mklink /D "%USERPROFILE%\.config\wezterm" "%current_dir%\wezterm" 
+
+mklink /D "%USERPROFILE%\Documents\PowerShell" "%current_dir%\PowerShell" 
+mklink /D "%USERPROFILE%\Documents\WindowsPowerShell" "%current_dir%\PowerShell" 
