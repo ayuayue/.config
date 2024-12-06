511b889471d9b4507b2ae9f00597562fb4cebac0
caoayu <1401262639@qq.com>
2024-12-06 10:01:48 +0800

    📦 NEW: mihomo

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,3 @@
-$env:HTTP_PROXY="http://127.0.0.1:7890"; $env:HTTPS_PROXY="http://127.0.0.1:7890"
+# $env:HTTP_PROXY="http://127.0.0.1:7890"; $env:HTTPS_PROXY="http://127.0.0.1:7890"
 Invoke-Expression "$(vfox activate pwsh)"
 Invoke-Expression (&starship init powershell)
